398df735ee574a09fdc89a1a3babd222570870da
Luis Grande <lgrande@greeneaglesolutions.com>
2024-02-22 09:07:08 +0100

    feat: 🎸 add dummy component

---
+++ src/app/dummy.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dummy-component',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class DummyComponent implements OnInit {
+
+  title = 'poc-ci';
+
+  ngOnInit(): void {
+    const variable = 'hola'
+
+    if (variable === 'hola') {
+        this.title = 'variable is hola'
+    } else {
+        this.title = 'variable is not hola'
+    }
+}
+}
